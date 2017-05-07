2f510eba140d37544c4cda9a9ab1964ea3a776be
Mateusz Łobocki <mlobocki@outlook.com>
2017-05-07 12:22:42 +0200

    Exercise model and service to get it

---
+++ PythonExercises/PythonExercises/Models/Exercise.cs
@@ -0,0 +1,20 @@
+﻿namespace PythonExercises.Models
+{
+    public class Exercise
+    {
+        public Exercise(int id, string question, string hints, string solution, string level)
+        {
+            Id = id;
+            Question = question;
+            Hints = hints;
+            Solution = solution;
+            Level = level;
+        }
+
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Hints { get; set; }
+        public string Solution { get; set; }
+        public string Level { get; set; }
+    }
+}
---
+++ PythonExercises/PythonExercises/Models/ExercisesService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Ajax.Utilities;
+using PythonExercises.Properties;
+
+namespace PythonExercises.Models
+{
+    public interface IExercisesService
+    {
+        IEnumerable<Exercise> Exercises { get; }
+        int Count { get; }
+        Exercise GetExercise(int id);
+    }
+
+    public class ExercisesService : IExercisesService
+    {
+        public ExercisesService()
+        {
+            Exercises = GetExercises();
+            Count = Exercises.Count();
+        }
+
+        public IEnumerable<Exercise> Exercises { get; }
+
+        public int Count { get; }
+
+        public Exercise GetExercise(int id)
+        {
+            return Exercises.First(x => x.Id == id);
+        }
+
+        protected IEnumerable<Exercise> GetExercises()
+        {
+            string resource = Resources.PythonCourseFile;
+            foreach (string exerciseRaw in resource.Split(new[] { "#----------------------------------------#" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (exerciseRaw.IsNullOrWhiteSpace())
+                {
+                    continue;
+                }
+
+                string[] lines = exerciseRaw.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] sections = exerciseRaw.Split(new[] { "Question:", "Hints:", "Solution:" }, StringSplitOptions.RemoveEmptyEntries);
+                int id = int.Parse(lines[0].Trim().Replace(":", "").Split(' ').Last());
+                string level = lines[1].Contains("Level") ? lines[1].Last().ToString() : "Unknown";
+                string question = sections[1].Trim();
+                string hints = sections[2].Trim();
+                string solution = sections[3].Trim();
+
+                yield return new Exercise(id, question, hints, solution, level);
+            }
+        }
+    }
+}
