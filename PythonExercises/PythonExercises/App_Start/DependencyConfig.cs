ec221b2f25284fafd3abf01aa26686b25652203e
Mateusz Łobocki <mlobocki@outlook.com>
2017-05-07 12:18:28 +0200

    Updated packages + added Dependency Injection

---
+++ PythonExercises/PythonExercises/App_Start/DependencyConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using PythonExercises.Models;
+
+namespace PythonExercises.App_Start
+{
+    public class DependencyConfig
+    {
+        public static void RegisterDependencyResolver()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<ExercisesService>().AsImplementedInterfaces();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
