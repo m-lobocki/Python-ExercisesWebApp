6d12405e504dd0c9adb8dbf09487d3f6f6286d6d
Mateusz Łobocki <mlobocki@outlook.com>
2017-05-09 18:17:14 +0200

    Exercises ready to implement in views

---
+++ PythonExercises/PythonExercises/Controllers/ExercisesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PythonExercises.Models;
+
+namespace PythonExercises.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        private readonly IExercisesService exercisesService;
+
+        public ExercisesController(IExercisesService exercisesService)
+        {
+            this.exercisesService = exercisesService;
+        }
+
+        public ActionResult Index()
+        {
+            var exercises = exercisesService.Exercises;
+
+            return View(exercises);
+        }
+
+        public ActionResult Show(int id)
+        {
+            Exercise exercise = exercisesService.GetExercise(id);
+            var viewModel = new ExerciseViewModel(exercise, exercisesService.Count);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ PythonExercises/PythonExercises/Models/ExerciseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PythonExercises.Models
+{
+    public class ExerciseViewModel
+    {
+        public ExerciseViewModel(Exercise exercise, int count)
+        {
+            Exercise = exercise;
+            Count = count;
+        }
+
+        public Exercise Exercise { get; set; }
+        public int Count { get; set; }
+    }
+}
